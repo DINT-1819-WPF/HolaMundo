cc8e2c390762723d3e8ca1c346aafcf5f96eced3
Javier Catalá <javicatala@gmail.com>
2018-12-07 15:00:55 +0100

    Agregar archivos de proyecto.

---
+++ HolaMundo/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class MainWindow
+    Private Sub AceptarButton_Click(sender As Object, e As RoutedEventArgs)
+        MessageBox.Show(If(HolaRadioButton.IsChecked, "Hola ", "Adiós ") & NombreTextBox.Text, "Hola Mundo", MessageBoxButton.OK, MessageBoxImage.Information)
+    End Sub
+End Class
